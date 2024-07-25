fe3cc4cad4507dd57f93482c278dad2e54b34057
Shin Dong Won <eastorigin21@gmail.com>
2024-07-25 15:45:24 +0900

    11일차 강의 내용

---
+++ src/com/ktdsuniversity/edu/extend/bar/AbstractBar.java
@@ -0,0 +1,82 @@
+package com.ktdsuniversity.edu.extend.bar;
+
+public abstract class AbstractBar {
+
+	protected int barMoney;
+	protected Beverage[] beverage;
+	protected Alcohol[] alcohol;
+	
+	public AbstractBar (int barMoney, Beverage[] beverage, Alcohol[] alcohol) {
+		this.barMoney = barMoney;
+		this.beverage = beverage;
+		this.alcohol = alcohol;
+	}
+	
+	public void increaseBeverageBarMoney(String beverageName, int orderNumber) {
+		for(int i = 0; i < beverage.length; i++) {
+			if(beverage[i].getBeverageName().equals(beverageName)) {
+				this.barMoney += beverage[i].getBeveragePrice() * orderNumber;
+			}
+		}
+	}
+	
+	public void increaseAlcoholBarMoney(String alcoholName, int orderNumber) {
+		for(int i = 0; i < alcohol.length; i++) {
+			if(alcohol[i].getAlcoholName().equals(alcoholName)) {
+				this.barMoney += alcohol[i].getAlcoholPrice() * orderNumber;
+			}
+		}
+	}
+	
+	public void beverageSell(String beverageName, Guest guest) {
+		for(int i = 0; i < beverage.length; i++) {
+			if(beverage[i].getBeverageName().equals(beverageName)) {
+				int beverageStock = beverage[i].getBeverageStock();
+				int guestBeverageOrderNumber = guest.getBeverageOrderNumber();
+				if(beverageStock < guestBeverageOrderNumber) {
+					guestBeverageOrderNumber = beverageStock;
+					beverage[i].decreaseBeverageStock(guestBeverageOrderNumber);
+					this.increaseBeverageBarMoney(beverageName, guestBeverageOrderNumber);
+					System.out.println("재고가 부족하여 현재 있는 재고만 판매하였습니다");
+					return;
+				}else {
+					beverage[i].decreaseBeverageStock(guestBeverageOrderNumber);
+					this.increaseBeverageBarMoney(beverageName, guestBeverageOrderNumber);
+					System.out.println("재고가 충분하여 " + beverageName + " " + guestBeverageOrderNumber + "개를 판매하였습니다.");
+				}
+				System.out.println("현재 바의 돈: " + this.barMoney + "원");
+				System.out.println("남은 " + beverageName + " 재고: " + beverage[i].getBeverageStock() + "개");
+				return;
+			}
+		}
+	}
+	
+	public abstract void alcoholSell(String alcoholName, Guest guest);
+	
+	public void processAlcoholSell(String alcoholName, Guest guest) {
+		for(int i = 0; i < alcohol.length; i++) {
+			if(alcohol[i].getAlcoholName().equals(alcoholName)) {
+				if(guest.isAdult() && !guest.isDrunken(alcoholName)) {
+					int alcoholStock = alcohol[i].getAlcoholStock();
+					int guesetAlcoholOrderNumber = guest.getAlcoholOrderNumber();
+					if(alcoholStock < guesetAlcoholOrderNumber) {
+						guesetAlcoholOrderNumber = alcoholStock;
+						alcohol[i].decreaseAlcoholStock(guesetAlcoholOrderNumber);
+						this.increaseAlcoholBarMoney(alcoholName, guesetAlcoholOrderNumber);
+						System.out.println("재고가 부족하여 현재 있는 재고만 판매하였습니다");
+						return;
+					}else {
+						alcohol[i].decreaseAlcoholStock(guesetAlcoholOrderNumber);
+						this.increaseAlcoholBarMoney(alcoholName, guesetAlcoholOrderNumber);
+						System.out.println("재고가 충분하여 " + alcoholName + " " + guesetAlcoholOrderNumber + "개를 판매하였습니다.");
+						System.out.println("남은 " + alcoholName + " 재고: " + alcohol[i].getAlcoholStock() + "개");
+					}
+					System.out.println("현재 바의 돈: " + this.barMoney + "원");
+				}else {
+					System.out.println("손님이 잔뜩 취해 있어 주류를 판매할 수 없습니다.");
+				}
+				return;
+			}
+		}
+	}
+}
---
+++ src/com/ktdsuniversity/edu/extend/bar/AdultBar.java
@@ -0,0 +1,18 @@
+package com.ktdsuniversity.edu.extend.bar;
+
+public class AdultBar extends AbstractBar{
+	
+	public AdultBar(int barMoney, Beverage[] beverage, Alcohol[] alcohol) {
+		super(barMoney, beverage, alcohol);
+	}
+	
+	@Override
+	public void alcoholSell(String alcoholName, Guest guest) {
+		if(!guest.isAdult()) {
+			System.out.println("성인만 입장 가능합니다");
+			return;
+		}
+		processAlcoholSell(alcoholName, guest);
+		
+	}
+}
